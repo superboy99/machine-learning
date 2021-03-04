b989d36426ee2698a097b2c9cb5962d244b4818f
superboy <pang105923@foxmail.com>
2021-03-04 08:34:50 +0800

    完成拟和直线

--- code/最小二乘法.py
+++ code/最小二乘法.py
@@ -3,17 +3,32 @@ import matplotlib.pyplot as plt
 
 # 1
 x = np.arange(0, 10, 0.2)
-yi = [5 * i + 3 for i in x]
-y = [np.random.normal(yi, 2) for yi in yi]
-# plt.plot(x,y,colar="red",linewidth=1)
+y_ = [5 * i + 3 for i in x]
+y = [np.random.normal(y_, 2) for y_ in y_]
 plt.scatter(x, y, c="c")
-plt.show()
+# plt.show()
 
 # 2
 x_bar = np.mean(x)
 y_bar = np.mean(y)
 
+xy_sum = 0
 x_sum = 0
-y_sum = 0
 
-for i in range(len(x)):
+print(x)
+print("--------------")
+print(y)
+
+n = range(len(x))
+
+for i in n:
+	xy_sum += x[i] * y[i]
+	x_sum += x[i] ** 2
+
+w = (xy_sum - n * x_bar * y_bar) / (x_sum - n * x_bar ** 2)
+b = y_bar - w * x_bar
+
+print(w, b)
+
+plt.plot(x, w * x + b, "r-")
+plt.show()
