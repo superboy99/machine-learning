bf68282c53a8967ee6b048add782539acb78c5d6
superboy <pang105923@foxmail.com>
2021-03-04 08:49:03 +0800

    完成拟合直线

--- code/最小二乘法.py
+++ code/最小二乘法.py
@@ -6,7 +6,6 @@ x = np.arange(0, 10, 0.2)
 y_ = [5 * i + 3 for i in x]
 y = [np.random.normal(y_, 2) for y_ in y_]
 plt.scatter(x, y, c="c")
-# plt.show()
 
 # 2
 x_bar = np.mean(x)
@@ -15,12 +14,7 @@ y_bar = np.mean(y)
 xy_sum = 0
 x_sum = 0
 
-print(x)
-print("--------------")
-print(y)
-
 n = range(len(x))
-
 for i in n:
 	xy_sum += x[i] * y[i]
 	x_sum += x[i] ** 2
@@ -28,8 +22,8 @@ for i in n:
 w = (xy_sum - n * x_bar * y_bar) / (x_sum - n * x_bar ** 2)
 b = y_bar - w * x_bar
 
-print("w=" + w, "b=" + b)
-print("拟合直线：" + "y=" + w + "*x" + b)
+print("w=",w, "b=", b)
+print("y=" + str(np.round_(w,2)) + "x+" + str(np.round_(b,2)))
 
 plt.plot(x, w * x + b, "r-")
 plt.show()
